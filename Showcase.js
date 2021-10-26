dad6e417decfb48f3d3e615df6169062f7a65688
sri8135 <93189545+sri8135@users.noreply.github.com>
2021-10-26 14:46:38 +0530

    Create Showcase.js

---
+++ Showcase.js
@@ -0,0 +1,17 @@
+# Travel-tourism-and-hospitality
+import React,{Component} from 'react'
+
+const Showcase = () => {
+  return (
+    <section className='showcase'>
+      <div className='showcase-overlay'>
+        <h1>Discover The World</h1>
+        <p>
+        Kick-off your beach season with a visit to the finest destinations, served with a smile : )
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Showcase
